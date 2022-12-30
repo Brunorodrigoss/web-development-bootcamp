fe926ad252d01269db47a30de961c95d13aebcf3
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2022-12-30 08:40:50 +0000

    introduction dom

--- projects/Dom Object Model/dom/index.js
+++ projects/Dom Object Model/dom/index.js
@@ -1,1 +1,10 @@
-document.querySelector("h1").innerHTML = "Good Bye";
+document.firstElementChild;
+document.lastElementChild;
+
+document.firstElementChild.firstElementChild;
+document.firstElementChild.lastElementChild;
+
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "Good Bye";
+heading.style.color = "red";
+document.querySelector("input").click();
