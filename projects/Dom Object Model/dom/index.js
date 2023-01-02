66a7cf2a58cf1e4ea8d3266f9305b9ca69ab4073
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-01-02 17:17:19 +0000

    selecting html elements with javascript

--- projects/Dom Object Model/dom/index.js
+++ projects/Dom Object Model/dom/index.js
@@ -8,3 +8,22 @@ var heading = document.firstElementChild.lastElementChild.firstElementChild;
 heading.innerHTML = "Good Bye";
 heading.style.color = "red";
 document.querySelector("input").click();
+
+document.getElementsByTagName("li");
+document.querySelector("li");
+document.querySelectorAll("li");
+document.getElementsByTagName("li")[2].style.color= "purple";
+
+
+document.getElementsByTagName("li").length;
+document.getElementsByClassName("btn");
+document.getElementById("title");
+
+document.getElementById("title");
+document.getElementById("title").innerHTML = "Heey!";
+document.querySelector("h1");
+document.querySelector("#title");
+document.querySelector(".btn");
+document.querySelector("li a");
+document.querySelector("li a");
+document.querySelector("li.item");
