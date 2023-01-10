17435db6a1f05753217c218f25adfcd657859441
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-01-10 08:56:07 +0000

    javascript objects

--- projects/Drum Kit/notes.js
+++ projects/Drum Kit/notes.js
@@ -20,3 +20,32 @@ function calculator(num1, num2, operator) {
 
 //debugger;
 //calculator(3,4,multiply);
+
+var houseKeeper1 = {
+    yearsOfExperience: 12,
+    name: "Jane",
+    cleaningRepertoire: ["bathroom", "lobby", "bedroom"]
+}
+
+console.log(houseKeeper1);
+
+
+// Constructor Function
+function HouseKeeper(yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+}
+
+function BellBoy(name, age, hasWorkPermit, languages) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
+}
+
+var bellboy1 = new BellBoy("Timmy", 19, true, ["French", "English"]);
+var houseKeeper2 = new HouseKeeper(9, "Leo", ["lobby", "bedroom"]);
+
+
+console.log(houseKeeper2.name)
