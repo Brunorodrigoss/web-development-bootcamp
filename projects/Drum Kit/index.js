d036677273a3600d09de5c26d96827faf55a1d4d
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-01-12 16:30:30 +0000

    add animation to website

--- projects/Drum Kit/index.js
+++ projects/Drum Kit/index.js
@@ -7,12 +7,14 @@ for (var index = 0; index < buttons.length; index++) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 // Detecting Keyboard Press
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -57,4 +59,14 @@ function makeSound(key) {
             console.log(key);
             break;
     }
+}
+
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
