049d441dbee31b22b4a08cad839f38acf21e73ee
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2023-01-17 08:26:28 +0000

    selecting elements with jquery

--- projects/Jquery/index.js
+++ projects/Jquery/index.js
@@ -1,3 +1,4 @@
 // document.querySelector("h1").style.color = "red"; === $("h1").css("color","red");
 
 $("h1").css("color","red");
+$("button").css("background-color", "yellow");
---
+++ projects/Jquery/notes.js
@@ -0,0 +1,5 @@
+$(document).ready(function() { 
+    $("h1").css("color","red");
+});
+
+//https://www.minifier.org/
